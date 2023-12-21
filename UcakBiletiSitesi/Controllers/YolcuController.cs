9a851d117135f5aecf394c01e9cec111146961c3
Sezer Bulgur <sezerbulgur2@gmail.com>
2023-12-22 01:04:51 +0300

    Tag Helper mantigi sisteme entegre edildi

---
+++ UcakBiletiSitesi/Controllers/YolcuController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UcakBiletiSitesi.Models;
+
+namespace UcakBiletiSitesi.Controllers
+{
+    public class YolcuController : Controller
+    {
+        IVeritabani Veritabani = new SanalVeritabani();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult KayitOl()
+        {
+            return View();
+        }
+
+        public IActionResult GirisYap() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Dogrula(Yolcu yolcu)
+        {
+            if (Veritabani.YolcuDogrula(yolcu))
+            {
+                //deneme olarak olusturulan bilet yolcuya aktarilmasi
+                Bilet b = new Bilet(yolcu);
+
+
+                HttpContext.Session.SetString("SessionUser", yolcu.Email);
+                return View("../Home/Deneme", yolcu);
+            }
+            ViewBag.Hata = "Hatali giris yapildi. Lutfen tekrar deneyiniz";
+            return View("GirisYap");
+        }
+    }
+}
--- UcakBiletiSitesi/Models/Yolcu.cs
+++ UcakBiletiSitesi/Models/Yolcu.cs
@@ -1,9 +1,12 @@
-﻿namespace UcakBiletiSitesi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UcakBiletiSitesi.Models
 {
     public class Yolcu : Kullanici
     {
         public string Ad { get; set; }
         public string Soyad { get; set; }
+        [Display(Name = "TC Kimlik Numarasi")]
         public string KimlikNo { get; set; }
         public List<Bilet> Biletler = new List<Bilet>();
     }
