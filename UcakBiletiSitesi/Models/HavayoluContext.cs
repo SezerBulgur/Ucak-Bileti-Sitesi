2ec3e6f921e8102e0009b2cdec59aac9ba58a43a
Sezer Bulgur <sezerbulgur2@gmail.com>
2023-12-27 05:41:12 +0300

    sql server sisteme entegre edildi. ilkMigration yapildi

---
+++ UcakBiletiSitesi/Models/HavayoluContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UcakBiletiSitesi.Models
+{
+    public class HavayoluContext: DbContext
+    {
+        public DbSet<Yolcu> Yolcular { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb; Database = Havayolu; Trusted_Connection = True");
+        }
+    }
+}
--- UcakBiletiSitesi/Models/Yolcu.cs
+++ UcakBiletiSitesi/Models/Yolcu.cs
@@ -4,9 +4,11 @@ namespace UcakBiletiSitesi.Models
 {
     public class Yolcu : Kullanici
     {
+        [Key] public string YolcuId { get; set; }
         public string Ad { get; set; }
         public string Soyad { get; set; }
         [Display(Name = "TC Kimlik Numarasi")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "Lutfen 11 haneli TC kimlik numaranizi giriniz")]
         public string KimlikNo { get; set; }
         public List<Bilet> Biletler = new List<Bilet>();
     }
